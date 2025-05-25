f5de5394f2b73b4d4a9104a8ed553cfcba7bd1e2
allumik <alvinmeltsov@gmail.com>
2025-05-25 15:59:37 +0200

    added my aurora sweep / miryoku keymap

---
+++ keyboards/splitkb/aurora/sweep/keymaps/allumik/config.h
@@ -0,0 +1,47 @@
+/* Copyright 2022 splitkb.com <support@splitkb.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+// necessary for the other side to work
+#define SPLIT_USB_DETECT
+
+// default but important
+#define TAPPING_TERM 165
+
+// add combos, update when adding more layers
+#define COMBO_TERM 160
+
+// add permissive hold for more flexible mod-alpha combinations
+#define PERMISSIVE_HOLD
+
+// Enable rapid switch from tap to hold, disables double tap hold auto-repeat.
+#define TAPPING_FORCE_HOLD
+
+// Recommended for heavy chording.
+#define QMK_KEYS_PER_SCAN 4
+
+// Mouse key speed and acceleration.
+#undef MOUSEKEY_DELAY
+#define MOUSEKEY_DELAY          0
+#undef MOUSEKEY_INTERVAL
+#define MOUSEKEY_INTERVAL       16
+#undef MOUSEKEY_WHEEL_DELAY
+#define MOUSEKEY_WHEEL_DELAY    0
+#undef MOUSEKEY_MAX_SPEED
+#define MOUSEKEY_MAX_SPEED      6
+#undef MOUSEKEY_TIME_TO_MAX
+#define MOUSEKEY_TIME_TO_MAX    64
---
+++ keyboards/splitkb/aurora/sweep/keymaps/allumik/keymap.c
@@ -0,0 +1,96 @@
+// generated from users/manna-harbour_miryoku/miryoku.org  -*- buffer-read-only: t -*-
+
+#include QMK_KEYBOARD_H
+
+#define KC_NP KC_NO // key is not present
+#define KC_NA KC_NO // present but not available for use
+#define KC_NU KC_NO // available but not used
+
+// like an onion
+enum layers { BASE, NAVR, MOUR, NSL, FUNL };
+
+// Don't forget to edit the rules.mk and config.h accordingly
+#define COMBO_COUNT 16
+enum combo_events {
+  OPEN_PAR, CLOSE_PAR, OPEN_BRA, CLOSE_BRA, GT, LT, MINUS, EQUAL,
+  SUPER_L, SUPER_R, LALT_L, LALT_R, ALGR_L, ALGR_R,
+  TAB, DEL
+};
+
+// some convinience combos
+const uint16_t PROGMEM opar_combo[]  = { KC_T, KC_G, COMBO_END };
+const uint16_t PROGMEM cpar_combo[]  = { KC_N, KC_M, COMBO_END };
+const uint16_t PROGMEM obra_combo[]  = { KC_D, KC_V, COMBO_END };
+const uint16_t PROGMEM cbra_combo[]  = { KC_H, KC_K, COMBO_END };
+const uint16_t PROGMEM gt_combo[] = { KC_L, KC_J, COMBO_END };
+const uint16_t PROGMEM lt_combo[] = { KC_P, KC_B, COMBO_END };
+const uint16_t PROGMEM minus_combo[] = { KC_B, KC_G, COMBO_END };
+const uint16_t PROGMEM equal_combo[] = { KC_J, KC_M, COMBO_END };
+
+// the modifier combos, super, lalgr and lalt
+const uint16_t PROGMEM super_left_combo[]  = { KC_T, KC_D, COMBO_END };
+const uint16_t PROGMEM super_right_combo[] = { KC_N, KC_H, COMBO_END };
+const uint16_t PROGMEM lalgr_left_combo[]   = { KC_G, KC_V, COMBO_END };
+const uint16_t PROGMEM lalgr_right_combo[]  = { KC_M, KC_K, COMBO_END };
+const uint16_t PROGMEM lalt_left_combo[]  = { KC_S, KC_C, COMBO_END };
+const uint16_t PROGMEM lalt_right_combo[] = { KC_E, KC_COMM, COMBO_END };
+
+// the "missing" thumb keys for Delete and Tab
+const uint16_t PROGMEM tab_combo[]  = { KC_Q, KC_W, COMBO_END };
+const uint16_t PROGMEM del_combo[] = { KC_Y, KC_QUOTE, COMBO_END };
+
+combo_t key_combos[COMBO_COUNT] = {
+  [OPEN_PAR]  = COMBO(opar_combo,  KC_LPRN),
+  [CLOSE_PAR] = COMBO(cpar_combo,  KC_RPRN),
+  [OPEN_BRA]  = COMBO(obra_combo,  KC_LBRC),
+  [CLOSE_BRA] = COMBO(cbra_combo,  KC_RBRC),
+  [GT]        = COMBO(gt_combo, KC_GT),
+  [LT]        = COMBO(lt_combo, KC_LT),
+  [MINUS] = COMBO(minus_combo, KC_MINS),
+  // could just add <plus> keycode but it's more functional here with shift + combo
+  [EQUAL] = COMBO(equal_combo, KC_EQL),
+
+  [SUPER_L] = COMBO(super_left_combo,  KC_LGUI),
+  [SUPER_R] = COMBO(super_right_combo, KC_LGUI),
+  [LALT_L]  = COMBO(lalt_left_combo,   KC_LALT),
+  [LALT_R]  = COMBO(lalt_right_combo,  KC_LALT),
+  [ALGR_L]  = COMBO(lalgr_left_combo,   KC_ALGR),
+  [ALGR_R]  = COMBO(lalgr_right_combo,  KC_ALGR),
+
+  [TAB] = COMBO(tab_combo, KC_TAB),
+  [DEL] = COMBO(del_combo, KC_DEL)
+};
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+  [BASE] = LAYOUT_split_3x5_2(
+    KC_Q,         KC_W,         KC_F, KC_P,              KC_B,              KC_J,              KC_L,            KC_U,    KC_Y,           KC_QUOT,
+    KC_A,         KC_R,         KC_S, KC_T,              KC_G,              KC_M,              KC_N,            KC_E,    KC_I,           KC_O,
+    LCTL_T(KC_Z), LSFT_T(KC_X), KC_C, KC_D,              KC_V,              KC_K,              KC_H,            KC_COMM, LSFT_T(KC_DOT), LCTL_T(KC_SLSH),
+                                      LT(NAVR, KC_SPC),  LT(MOUR, KC_ESC),  LT(FUNL, KC_ENT),  LT(NSL, KC_BSPC)
+  ),
+
+  [NAVR] = LAYOUT_split_3x5_2(
+    KC_NA,   KC_NA,   KC_NA,    KC_NA,   KC_NA,   KC_CAPS, KC_UNDO, KC_CUT,  KC_COPY, KC_PSTE,
+    KC_NA,   KC_NA,   KC_NA,    KC_NA,   KC_NA,   KC_AGIN, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT,
+    KC_LCTL, KC_LSFT, KC_LALT,  KC_LGUI, KC_ALGR, KC_INS,  KC_HOME, KC_PGDN, KC_PGUP, KC_END,
+                                KC_NA,   KC_NA,   KC_ENT,  KC_BSPC
+  ),
+  [MOUR] = LAYOUT_split_3x5_2(
+    KC_NA,   KC_NA,   KC_NA,    KC_NA,   KC_NA,   KC_NU,   KC_NU,   KC_MS_U, KC_NU,   KC_NU,
+    KC_NA,   KC_NA,   KC_NA,    KC_NA,   KC_NA,   KC_NU,   KC_MS_L, KC_MS_D, KC_MS_R, KC_NU,
+    KC_LCTL, KC_LSFT, KC_LALT,  KC_LGUI, KC_ALGR, KC_BTN3, KC_WH_L, KC_WH_D, KC_WH_U, KC_WH_R,
+                                KC_NA,   KC_NA,   KC_BTN1, KC_BTN2
+  ),
+  [FUNL] = LAYOUT_split_3x5_2(
+    KC_F12, KC_F7, KC_F8, KC_F9,  KC_PSCR, KC_NU,   KC_NU,   KC_MPRV, KC_MNXT, KC_NU,
+    KC_F11, KC_F4, KC_F5, KC_F6,  KC_SLCT, KC_MSTP, KC_MPLY, KC_VOLD, KC_VOLU, KC_MUTE,
+    KC_F10, KC_F1, KC_F2, KC_F3,  KC_PAUS, KC_ALGR, KC_LGUI, KC_LALT, KC_LSFT, KC_LCTL,
+                          KC_TAB, KC_ESC,  KC_NA,   KC_NA
+  ),
+  [NSL] = LAYOUT_split_3x5_2(
+    KC_LBRC, KC_7, KC_8,  KC_9, KC_RBRC, KC_NA,   KC_NA,   KC_NA,   ALGR(KC_LBRC), ALGR(KC_RBRC),
+    KC_SCLN, KC_4, KC_5,  KC_6, KC_EQL,  KC_NA,   KC_NA,   KC_NA,   ALGR(KC_SCLN), ALGR(KC_QUOT),
+    KC_GRV,  KC_1, KC_2,  KC_3, KC_BSLS, KC_ALGR, KC_LGUI, KC_LALT, KC_LSFT,       KC_LCTL,
+                          KC_0, KC_MINS, KC_NA,   KC_NA
+  )
+};
