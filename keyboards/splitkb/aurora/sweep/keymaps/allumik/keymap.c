559cdf7b63b1da87a4166dca080e7809c34c8571
allumik <alvinmeltsov@gmail.com>
2025-08-20 21:53:35 +0200

    experimenting with the layers

--- keyboards/splitkb/aurora/sweep/keymaps/allumik/keymap.c
+++ keyboards/splitkb/aurora/sweep/keymaps/allumik/keymap.c
@@ -7,14 +7,15 @@
 #define KC_NU KC_NO // available but not used
 
 // like an onion
-enum layers { BASE, NAVR, MOUR, NSL, FUNL, GAMEL, GAMEPLUSL };
+enum layers { BASE, GAMEL, GAMEPLUSL, NAVR, MOUR, NSL, FUNL };
 
 // Don't forget to edit the rules.mk and config.h accordingly
-#define COMBO_COUNT 16
+#define COMBO_COUNT 17
 enum combo_events {
   OPEN_PAR, CLOSE_PAR, OPEN_BRA, CLOSE_BRA, GT, LT, MINUS, EQUAL,
   SUPER_L, SUPER_R, LALT_L, LALT_R, ALGR_L, ALGR_R,
-  TAB, DEL
+  TAB, DEL,
+  GAMEM
 };
 
 // some convinience combos
@@ -39,6 +40,9 @@ const uint16_t PROGMEM lalt_right_combo[] = { KC_E, KC_COMM, COMBO_END };
 const uint16_t PROGMEM tab_combo[]  = { KC_Q, KC_W, COMBO_END };
 const uint16_t PROGMEM del_combo[] = { KC_Y, KC_QUOTE, COMBO_END };
 
+// combo for the game layer
+const uint16_t PROGMEM game_mode[] = { KC_B, KC_J, COMBO_END };
+
 combo_t key_combos[COMBO_COUNT] = {
   [OPEN_PAR]  = COMBO(opar_combo,  KC_LPRN),
   [CLOSE_PAR] = COMBO(cpar_combo,  KC_RPRN),
@@ -58,7 +62,10 @@ combo_t key_combos[COMBO_COUNT] = {
   [ALGR_R]  = COMBO(lalgr_right_combo,  KC_ALGR),
 
   [TAB] = COMBO(tab_combo, KC_TAB),
-  [DEL] = COMBO(del_combo, KC_DEL)
+  [DEL] = COMBO(del_combo, KC_DEL),
+
+  // activate the GAME MODE!!! WOOW
+  [GAMEM] = COMBO(game_mode, DF(GAMEL)),
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -85,7 +92,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_F12, KC_F7, KC_F8, KC_F9,  KC_PSCR, KC_NU,   KC_NU,   KC_MPRV, KC_MNXT, KC_NU,
     KC_F11, KC_F4, KC_F5, KC_F6,  KC_SLCT, KC_MSTP, KC_MPLY, KC_VOLD, KC_VOLU, KC_MUTE,
     KC_F10, KC_F1, KC_F2, KC_F3,  KC_PAUS, KC_ALGR, KC_LGUI, KC_LALT, KC_LSFT, KC_LCTL,
-                          KC_TAB, KC_ESC,  KC_NA,   TG(GAMEL)
+                          KC_TAB, KC_ESC,  KC_NA,   KC_NA
   ),
   [NSL] = LAYOUT_split_3x5_2(
     KC_LBRC, KC_7, KC_8,  KC_9, KC_RBRC, KC_NA,   KC_NA,   KC_NA,   ALGR(KC_LBRC), ALGR(KC_RBRC),
@@ -101,9 +108,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                          LT(GAMEPLUSL, KC_SPC), KC_LALT, LT(FUNL, KC_ENT), LT(NSL, KC_BSPC)
   ),
   [GAMEPLUSL] = LAYOUT_split_3x5_2(
-    KC_ESC,  KC_TRNS, KC_TRNS, KC_TRNS, KC_6,    KC_PGUP,          KC_EQL,          KC_GRV,  KC_P,   KC_QUOT,
-    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_5,    KC_HOME,          KC_MINS,         KC_SCLN, KC_L,   KC_BSLS,
-    KC_TRNS, KC_1,    KC_2,    KC_3,    KC_4,    KC_PGDN,          KC_RBRC,         KC_LBRC, KC_DOT, KC_SLSH,
-                               KC_NA,   KC_LALT, LT(FUNL, KC_ENT), LT(NSL, KC_BSPC)
-  )
+    KC_ESC,  KC_TRNS, KC_TRNS, KC_TRNS, KC_6,     KC_PGUP, KC_EQL,  KC_GRV,  KC_P,   KC_QUOT,
+    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_5,     KC_HOME, KC_MINS, KC_SCLN, KC_L,   KC_BSLS,
+    KC_TRNS, KC_1,    KC_2,    KC_3,    KC_4,     KC_PGDN, KC_RBRC, KC_LBRC, KC_DOT, KC_SLSH,
+                               KC_NA,   DF(BASE), KC_ENT,  KC_BSPC
+  ),
+
 };
+
+bool combo_should_trigger(uint16_t combo_index, combo_t *combo, uint16_t keycode, keyrecord_t *record) {
+  // Check if the current default layer is the GAMEL layer.
+  if (biton32(default_layer_state) == GAMEL) {
+    // If it is, do not allow any combos to trigger.
+    return false;
+  }
+
+  // For all other layers, allow combos to trigger as normal.
+  return true;
+}
